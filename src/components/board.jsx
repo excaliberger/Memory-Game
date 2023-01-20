44f48c743017afc971dfe3e4b71e9431cc972c50
Emily Berger <excaliberger@gmail.com>
2023-01-20 15:20:47 -0500

    update

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -4,26 +4,90 @@ import Cards from './cards.jsx'
 class Board extends Component {
     constructor (props) {
         super(props);
+
+
+        this.state = {
+            firstCard: "",
+            lastCard: "",
+            firstUniqueId: "",
+            lastUniqueId: "",
+            score: 0,
+        }
     }
     
+    shuffle() {
+        let idArr = ["ID1","ID1", "ID2", "ID2", "ID3", "ID3", "ID4", "ID4",
+        "ID5", "ID5", "ID6", "ID6", "ID7", "ID7", "ID8", "ID8"];
+        let shuffledCards = [];
+        let tempCards = [];
+        while (tempCards.length < 16) {
+          let a = Math.floor(Math.random() * 16);
+      
+          if (tempCards.includes(a) == false && a < 16) {
+            tempCards.push(a);
+          }
+        }
+        // console.log(tempCards);
+        for (let i = 0; i < 16; i++) {
+          shuffledCards.push(idArr[tempCards[i]]);
+        }
+      
+        // console.log(shuffledCards);
+        return shuffledCards;
+    };
+
+    foundMatch(id, uniqueId) {
+        if (this.state.firstCard === "") {
+            this.state.firstCard = id;
+            this.state.firstUniqueId = uniqueId;
+        } else {
+            this.state.lastCard = id;
+            this.state.lastUniqueId = uniqueId;
+            if (this.state.firstCard === this.state.lastCard) {
+                console.log("you found a match! Huzzah");
+                this.state.score++;
+                console.log(this.state.score);
+            } else {
+                // id.setState({className: "cards"});
+                let firstCard = document.getElementById(this.state.firstUniqueId)
+                let secondCard = document.getElementById(this.state.lastUniqueId)
+                firstCard.className = `cards ${this.matchId}`
+                secondCard.className = `cards ${this.matchID}`
+                // firstCard.setTimeout(myFunction, 3000)
+                
+                // add time delay so you can see both cards before they automatically flip back
+            }
+            this.state.firstCard = "";
+            this.state.lastCard = "";
+        }
+    }
+
+        
     render () {
-        let cardArr = [...Array(4).keys()]
+        this.shuffle()
+        let shuffleDeck = this.shuffle();
+        let cardArr = [0, 1 ,2 ,3];
+        let x = 0;
+        let y = 4;
+        let z = 8;
+        let q = 12;
+        
         let cardRows = cardArr.map((i, index) => {
-            return <><div className="cardContainer">
-            <Cards index={index}/>
-            <Cards index={index+4}/>
-            <Cards index={index+8}/>
-            <Cards index={index+12}/>
-            <Cards index={index+16}/>
-            </div>
-            </>          
+            return <div className="cardContainer" key={`rowKey${index}`}>
+            <Cards index={index} matchId={`${shuffleDeck[x++]}`} foundMatch= {(id, uniqueId) => {this.foundMatch(id, uniqueId)}}/>
+            <Cards index={index+4} matchId={`${shuffleDeck[y++]}`} foundMatch={(id, uniqueId) => {this.foundMatch(id, uniqueId)}}/>
+            <Cards index={index+8}  matchId={`${shuffleDeck[z++]}`} foundMatch={(id, uniqueId) => {this.foundMatch(id, uniqueId)}}/>
+            <Cards index={index+12} matchId={`${shuffleDeck[q++]}`} foundMatch={(id, uniqueId) => {this.foundMatch(id, uniqueId)}}/>
+            </div>      
         });
-        console.log(cardArr);
+        // console.log(cardArr);
         return <div id="gameBoard">
                     {cardRows}
                </div>
     }
 
-}
+
+    }
+
 
 export default Board;
--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -5,28 +5,47 @@ class Cards extends Component {
         super(props);
 
         this.state = {
-            className: "cards",
-            id: `cardDiv${this.props.index}`
+            // className: "cards",
+            matchId: `${this.props.matchId}`,
+            id: `${this.props.index}`,
+            lastCard: "",
+            remainingMismatches: []
         };
+    }
+
+    chancesRemaining() {
+        //how to calculate mismatches made
     }
 
     render() {
-        return (<div id={this.state.id} onClick={() => {this.cardFlipOnClick()}} className={this.state.className}>
+        return (<div 
+            id={this.state.id} 
+            onClick={() => {this.cardFlipOnClick()}} 
+            className={"cards" + " " + this.state.matchId}
+            > {this.state.matchId}
         </div>);
     }
 
-    cardFlipOnClick() {
-        console.log(this);
-        let card = document.getElementById(this.state.id);
-        if (card.className === "cards") { 
-            this.setState({className: "cards-flipped"});
-            card.textContent = `${card.id}`
-        } else if (card.className === "cards-flipped") {
-            this.setState({className: "cards"});
-            card.textContent = '';
 
+    cardFlipOnClick() {
+        // console.log(this);
+        let card = document.getElementById(this.state.id);
+        // console.log("here", card.className, this.state.className)
+        // if (card.className !== this.state.className) {
+        //     this.state.className = `${card.className}`
+        //     console.log(this.state.className, card.className)
+        // } 
+        if (!card.className.includes("flipped")) { 
+            this.setState({className: "cards-flipped"});
+            card.className = "cards-flipped" +" "+ this.state.matchId
+            card.textContent = `${this.state.matchId}`;
+            this.props.foundMatch(this.state.matchId, this.state.id);
+        } else {
+            // this.setState({className: "cards"});
+            // this.props.foundMatch(this.state.matchId, this.state.id);
+            // card.textContent = '';
         };
-    };
+    }
 }
 
 export default Cards;
